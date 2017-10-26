02600befe90e62d1a9c08e6042e3377a9487c266
Biplab Das <31961556+Biplabboss@users.noreply.github.com>
2017-10-26 18:34:08 +0530

    Update weeman.sh

--- weeman.sh
+++ weeman.sh
@@ -1,8 +1,27 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
+echo -e "Updating default packages\n"
+apt update && apt -y upgrade
 
+pkg install -y ruby
+gem install lolcat
 
+pkg install -y toilet
+pkg install figlet
+
+toilet -f mono12 -F border "Biplab"
+toilet -f term -F border -F metal "Follow my social handle wp- +1(518) 350-6393"
+sleep 10
+
+pkg install -y python2
 git clone https://github.com/evait-security/weeman.git
 sleep 5
-cd biplab/weeman
-python2 weeman.py
+
+cd biplab
+mv -v weeman /$HOME
+cd
+rm -rf biplab
+
+figlet -f big "Biplab Das" | lolcat
+echo -e "\n"
+echo -e "successfully install weeman\n"
